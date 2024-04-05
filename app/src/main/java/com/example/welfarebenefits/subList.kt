bbbd3fc35cbfa7c2d9e890fd91459f24bc13952b
noah-wilson0 <127368908+noah-wilson0@users.noreply.github.com>
2024-04-05 21:32:42 +0900

    create recently layout

---
+++ app/src/main/java/com/example/welfarebenefits/subList.kt
@@ -0,0 +1,11 @@
+package com.example.welfarebenefits
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class subList : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sub_list)
+    }
+}
