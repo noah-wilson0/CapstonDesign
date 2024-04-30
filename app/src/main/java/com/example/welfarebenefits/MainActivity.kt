e87dbc0c80b0c615c82f4ea23ea12435db51ac92
noah-wilson0 <127368908+noah-wilson0@users.noreply.github.com>
2024-04-30 09:49:24 +0900

    drop down menu test

--- app/src/main/java/com/example/welfarebenefits/MainActivity.kt
+++ app/src/main/java/com/example/welfarebenefits/MainActivity.kt
@@ -1,21 +1,43 @@
 package com.example.welfarebenefits
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.welfarebenefits.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private var mBinding:ActivityMainBinding?=null
+    private var mBinding: ActivityMainBinding?=null
     private val binding get() = mBinding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_main)
         mBinding= ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        
 
+        val sortingCriteriaArray = resources.getStringArray(R.array.sorting_criteria)
 
+        var spinnerAdapter =
+            ArrayAdapter(this@MainActivity, android.R.layout.simple_spinner_item, sortingCriteriaArray)
+        binding.mainListSelectSpinner.adapter = spinnerAdapter
+
+        binding.mainListSelectSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                // 선택된 아이템의 위치(position)에 대한 처리를 여기에 추가.
+                val selectedItem = sortingCriteriaArray[position]
+                // 선택된 아이템에 대한 이벤트 추가
+
+                //예시
+                Toast.makeText(this@MainActivity, selectedItem, Toast.LENGTH_SHORT).show()
+
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                // 아무것도 선택되지 않았을 때의 이벤트
+            }
+        }
 
     }
 }
