6f5e85523378ba85cb6e42f8be05bc8b1a2e4b5e
noah-wilson0 <127368908+noah-wilson0@users.noreply.github.com>
2024-05-02 15:34:16 +0900

    login binding

--- app/src/main/java/com/example/welfarebenefits/activity/LogIn.kt
+++ app/src/main/java/com/example/welfarebenefits/activity/LogIn.kt
@@ -2,11 +2,18 @@ package com.example.welfarebenefits.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.welfarebenefits.R
+import com.example.welfarebenefits.databinding.ActivityLogInBinding
 
 class LogIn : AppCompatActivity() {
+    private var mBinding:ActivityLogInBinding?=null
+    private val binding get() = mBinding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_log_in)
+
+        mBinding= ActivityLogInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
