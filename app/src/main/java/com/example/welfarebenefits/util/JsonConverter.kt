9be7dffef364d361b2e071e9da8fc7b1fe390eda
noah-wilson0 <127368908+noah-wilson0@users.noreply.github.com>
2024-05-06 19:36:08 +0900

    add JsonConverter

---
+++ app/src/main/java/com/example/welfarebenefits/util/JsonConverter.kt
@@ -0,0 +1,16 @@
+package com.example.welfarebenefits.util
+
+import com.example.welfarebenefits.entity.User
+import com.google.gson.Gson
+
+
+class JsonConverter {
+    private val gson = Gson()
+
+    fun userToJson(obj:Any):String {
+        return gson.toJson(obj)
+    }
+    fun jsonToUser(json: String):User {
+        return gson.fromJson(json,User::class.java )
+    }
+}
