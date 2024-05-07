2890e9fc61cf546aa1b8f5c5c08d4a1728a5df12
noah-wilson0 <127368908+noah-wilson0@users.noreply.github.com>
2024-05-07 10:30:40 +0900

    update ActivityStarter

--- app/src/main/java/com/example/welfarebenefits/util/ActivityStarter.kt
+++ app/src/main/java/com/example/welfarebenefits/util/ActivityStarter.kt
@@ -2,18 +2,22 @@ package com.example.welfarebenefits.util
 
 import android.app.Activity
 import android.content.Intent
-import com.example.welfarebenefits.activity.UserInfoActivity
 import com.example.welfarebenefits.entity.User
 
 
 //메서드 분리하기 (인텐트, json변환메서드)
 class ActivityStarter {
     companion object {
-        fun startNextActivity(activity: Activity, user: User) {
+        fun startNextActivity(activity: Activity,nextActivity:Class<*>, user: User) {
             val userJson = JsonConverter().userToJson(user)
-            val intent = Intent(activity, UserInfoActivity::class.java)
+            val intent = Intent(activity, nextActivity)
             intent.putExtra("user", userJson)
             activity.startActivity(intent)
         }
+        fun startNextActivity(activity: Activity,nextActivity:Class<*>){
+            val intent = Intent(activity, nextActivity)
+            activity.startActivity(intent)
+        }
+
     }
 }
