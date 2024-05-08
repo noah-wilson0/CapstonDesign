d7e65cfb09ed10250146d3321494902ff4a18a20
noah-wilson0 <127368908+noah-wilson0@users.noreply.github.com>
2024-05-08 10:54:50 +0900

    id중복 처리완료

--- app/src/main/java/com/example/welfarebenefits/activity/SignUpActivity.kt
+++ app/src/main/java/com/example/welfarebenefits/activity/SignUpActivity.kt
@@ -2,16 +2,16 @@ package com.example.welfarebenefits.activity
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.example.welfarebenefits.databinding.ActivitySignUpBinding
 import com.example.welfarebenefits.util.ShowAlertDialog
-import com.example.welfarebenefits.util.ShowAlertDialogListener
 import com.example.welfarebenefits.util.SignUp
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.auth
-
+/*
+    firebase는 email만 검증가능
+ */
 class SignUpActivity : AppCompatActivity() {
     private var mBinding: ActivitySignUpBinding?=null
     private val binding get() = mBinding!!
@@ -24,45 +24,49 @@ class SignUpActivity : AppCompatActivity() {
         val intent=Intent(this,LogInActivity::class.java)
         auth = Firebase.auth
 
-
         binding.submit.setOnClickListener {
-
-            var success:Boolean=SignUp(this,binding).signUp()
-            when (success) {
-                true -> {
-                    ShowAlertDialog(this,
-                        "회원가입",
-                        "회원가입이 완료되었습니다!",
-                        "확인",
-                        listener = object : ShowAlertDialogListener {
-                            override fun onPositiveButtonClicked() {
-                                startActivity(intent)
-                                finish()
-                            }
-
-                            override fun onNegativeButtonClicked() {
-
-                            }
-                        }).showAlertDialog()
-                    val currentUser = auth.currentUser
-                    if (currentUser != null) {
-                        Log.e("SignUpActivity","회원가입후currentuser는 null이 아님")
-                        Log.e("SignUpActivity",currentUser.toString())
-                    }
-                    else
-                    {
-                        Log.e("SignUpActivity","회원가입후currentuser는 null임")
-                    }
+            when {
+                binding.inputIDET.text.toString().isBlank() && binding.inputIDET.text.toString()
+                    .isNullOrEmpty() -> {
+                    ShowAlertDialog(this, "회원가입", "아이디를 입력해 주세요!", "확인").showAlertDialog()
                 }
-                false -> {
-                    ShowAlertDialog(this,"회원가입","회원가입에 실패했습니다. 죄송합니다. 다시 시도해주세요.","확인").showAlertDialog()
+
+                binding.inputPSET.text.toString().isBlank() && binding.inputPSET.text.toString()
+                    .isNullOrEmpty() -> {
+                    ShowAlertDialog(this, "회원가입", "비밀번호를 입력해 주세요!", "확인").showAlertDialog()
                 }
-                else -> {
-                    moveTaskToBack(true);						// 태스크를 백그라운드로 이동
-                    finishAndRemoveTask();						// 액티비티 종료 + 태스크 리스트에서 지우기
-                    android.os.Process.killProcess(android.os.Process.myPid());	// 앱 프로세스 종료
+
+                binding.inputNameET.text.toString().isBlank() && binding.inputNameET.text.toString()
+                    .isNullOrEmpty() -> {
+                    ShowAlertDialog(this, "회원가입", "이름을 입력해 주세요!", "확인").showAlertDialog()
                 }
-            }
+
+                binding.inputAvgIncomeSesstionET.text.toString()
+                    .isBlank() && binding.inputAvgIncomeSesstionET.text.toString()
+                    .isNullOrEmpty() -> {
+                    ShowAlertDialog(this, "회원가입", "연 평균 소득을 입력해 주세요!", "확인").showAlertDialog()
+                }
+
+                binding.familyStructureSesstionET.text.toString()
+                    .isBlank() && binding.familyStructureSesstionET.text.toString()
+                    .isNullOrEmpty() -> {
+                    ShowAlertDialog(this, "회원가입", "가족관계를 입력해 주세요!", "확인").showAlertDialog()
+                }
+
+                binding.residenceSesstionET.text.toString()
+                    .isBlank() && binding.residenceSesstionET.text.toString().isNullOrEmpty() -> {
+                    ShowAlertDialog(this, "회원가입", "거주지역을 입력해 주세요!", "확인").showAlertDialog()
+                }
+
+                binding.inputIDET.text.toString().isNotBlank()
+                        && binding.inputPSET.text.toString().isNotBlank()
+                        && binding.inputNameET.text.toString().isNotBlank()
+                        && binding.inputAvgIncomeSesstionET.text.toString().isNotBlank()
+                        && binding.familyStructureSesstionET.text.toString().isNotBlank()
+                        && binding.residenceSesstionET.text.toString().isNotBlank() -> {
+                            SignUp(this, binding).signUp()
+                        }
+                }
         }
         binding.cancel.setOnClickListener {
             startActivity(intent)
--- app/src/main/java/com/example/welfarebenefits/util/SignUp.kt
+++ app/src/main/java/com/example/welfarebenefits/util/SignUp.kt
@@ -2,6 +2,7 @@ package com.example.welfarebenefits.util
 
 
 import android.util.Log
+import com.example.welfarebenefits.activity.LogInActivity
 import com.example.welfarebenefits.activity.SignUpActivity
 import com.example.welfarebenefits.databinding.ActivitySignUpBinding
 import com.example.welfarebenefits.entity.User
@@ -10,19 +11,31 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
+/*
+ //아이디 중복인경우 : return false ->(db 추가 안됨)
+ //비밀번호 중복인 경우: 검토 안함 -> return이 뭔지 확인불가 ->  db와 auth에 추가됨->"이미 사용중인 아이디입니다." 다이얼로그
+  //이미 로그인한 유저인 경우 -> 처리해야됨
+
+ */
+
 
 class SignUp (val signUpActivity: SignUpActivity, private val binding: ActivitySignUpBinding){
     private lateinit var auth: FirebaseAuth
-    private  var success=false
+//    private  var success:Boolean=false
 
-    fun signUp():Boolean{
+    fun signUp(){
         auth = Firebase.auth
         val currentUser = auth.currentUser
         if (currentUser != null) {
             Log.e("signUp","이미 로그인한 유저:회원가입 불가능")
+            ShowAlertDialog(
+                signUpActivity,
+                "회원가입",
+                "이미 가입된 사용자입니다. 이미 가입한 계정으로 로그인해주세요",
+                "확인"
+            ).showAlertDialog()
         }
         else {
-            success=true
             val user: User = User(
                 binding.inputIDET.text.toString().trim(),
                 binding.inputPSET.text.toString().trim(),
@@ -43,10 +56,42 @@ class SignUp (val signUpActivity: SignUpActivity, private val binding: ActivityS
                 ).getCheckedTexts()
             )
             Log.e("SignUp", "회원가입 진행")
-            NewUser().writeDatabaseNewUser(user)
-            NewUser().addNewUserAccount(user, signUpActivity)
+
+//            NewUser().addNewUserAccount(user, signUpActivity)
+//            NewUser().writeDatabaseNewUser(user)
+            NewUser().addNewUserAccount(user, signUpActivity, object : NewUser.OnUserAccountAddedListener {
+                override fun onUserAccountAdded() {
+                    // 회원가입이 성공한 경우에만 데이터베이스에 사용자 데이터를 추가
+                    NewUser().writeDatabaseNewUser(user)
+//                    success = true
+                    ShowAlertDialog(signUpActivity,
+                        "회원가입",
+                        "회원가입이 완료되었습니다!",
+                        "확인",
+                        listener = object : ShowAlertDialogListener {
+                            override fun onPositiveButtonClicked() {
+                               ActivityStarter.startNextActivity(signUpActivity,LogInActivity::class.java)
+                            }
+
+                            override fun onNegativeButtonClicked() {
+
+                            }
+                        }).showAlertDialog()
+                }
+                override fun onUserAccountAddFailed() {
+                    // 회원가입 실패한 경우
+//                    success = false
+                    ShowAlertDialog(
+                        signUpActivity,
+                        "회원가입",
+                        "이미 사용중인 아이디입니다.",
+                        "확인"
+                    ).showAlertDialog()
+                }
+            })
+
         }
-        return success
+//        return success
 
     }
 
